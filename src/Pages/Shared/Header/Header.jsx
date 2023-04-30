d04de3f7f3bc0894e4c507945b752fba638b8a0d
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-30 23:01:21 +0600

    primary setup done

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -4,6 +4,9 @@ import moment from "moment";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Header = () => {
   return (
@@ -22,6 +25,26 @@ const Header = () => {
           the original series, was commissioned for a follow-up season shortly
           after its successful debut in 2021.
         </Marquee>
+      </div>
+      <div className="mt-2 rounded">
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+          <Container>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mx-auto">
+                <Nav.Link href="#features">Home</Nav.Link>
+                <Nav.Link href="#pricing">About</Nav.Link>
+                <Nav.Link href="#pricing">Career</Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">Profile</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                  <Button variant="secondary">Login</Button>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </div>
     </Container>
   );
--- src/Pages/Shared/RightNav/RightNav.jsx
+++ src/Pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,45 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import Qzone from "../QZone/Qzone";
+import bg from "../../../assets/bg.png";
 
 const RightNav = () => {
   return (
     <div>
-      <h2>This is rightnav</h2>
+      <h4 className="mt-4">Login With</h4>
+      <Button className="mb-2" variant="outline-primary">
+        <FaGoogle></FaGoogle> Login with google
+      </Button>
+      <Button variant="outline-secondary">
+        <FaGithub></FaGithub> Login with github
+      </Button>
+      <div className="mt-4">
+        <h4>Find Us on</h4>
+
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebook></FaFacebook> FaceBook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter></FaTwitter> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram></FaInstagram> Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <Qzone></Qzone>
+      <div>
+        <img src={bg} alt="" />
+      </div>
     </div>
   );
 };
