6d6d1b3f03d11c0a239ad559250df3d66e843d10
Sihab Hossain <sihabhossain380@gmail.com>
2023-05-01 13:22:39 +0600

    recap done

--- src/Pages/Shared/LeftNav/LeftNav.jsx
+++ src/Pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
   return (
     <div>
-      <h2>This is leftNav</h2>
+      <h4>All Category</h4>
+      <div className="ps-4">
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link
+              to={`/category/${category.id}`}
+              className="text-decoration-none text-secondary"
+            >
+              {category.name}
+            </Link>
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
